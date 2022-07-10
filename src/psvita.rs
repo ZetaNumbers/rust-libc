bf182c10b53f80be36549d15dfd2cbc40c07eb8c
Daria Sukhonina <dariasukhonina@gmail.com>
2022-07-10 18:15:50 +0300

    feat: add some psvita sce types

--- src/psvita.rs
+++ src/psvita.rs
@@ -43,3 +43,64 @@ cfg_if! {
         }
     }
 }
+
+pub type SceChar8 = i8;
+pub type SceUChar8 = u8;
+
+pub type SceInt8 = i8;
+pub type SceUInt8 = u8;
+
+pub type SceShort16 = i16;
+pub type SceUShort16 = u16;
+
+pub type SceInt16 = i16;
+pub type SceUInt16 = u16;
+
+pub type SceInt32 = i32;
+pub type SceUInt32 = u32;
+
+pub type SceInt = i32;
+pub type SceUInt = u32;
+
+pub type SceInt64 = i64;
+pub type SceUInt64 = u64;
+
+pub type SceLong64 = i64;
+pub type SceULong64 = u64;
+
+pub type SceSize = usize;
+pub type SceSSize = isize;
+
+pub type SceBool = i32;
+pub const SCE_FALSE: SceBool = 0;
+pub const SCE_TRUE: SceBool = 1;
+
+pub type SceFloat = f32;
+pub type SceFloat32 = f32;
+
+pub type SceDouble = f64;
+pub type SceDouble64 = f64;
+
+pub type SceSByte = i8;
+pub type SceSByte8 = i8;
+
+pub type SceByte = u8;
+pub type SceByte8 = u8;
+
+pub type SceWChar16 = u16;
+pub type SceWChar32 = u32;
+
+pub type SceVoid = c_void;
+pub type ScePVoid = *mut c_void;
+
+pub type SceIntPtr = isize;
+pub type SceUIntPtr = usize;
+pub type SceUIntVAddr = usize;
+
+pub type SceOff = SceInt64;
+
+pub type SceUID = i32;
+
+pub type SceName = *mut c_char;
+
+pub const SCE_UID_NAMELEN: usize = 31;
